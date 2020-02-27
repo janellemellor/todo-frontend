0add17dfd528a50f11a357390b491dd70a0f6398
janelle mellor <janellemellor@gmail.com>
2020-02-27 12:02:10 -0800

    start rendering todo list from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import TodoApp from './TodoApp.js';
 import './App.css';
+import { 
+    Route,  
+    Switch,
+    Link, 
+    //may need to add redirect too
+    BrowserRouter as Router, 
+} from 'react-router-dom';
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/:storedTodos?" component={TodoApp} />
+
+          </Switch>
+          
+        </div>
+      </Router>
     )
   }
 }
+
 
 //will have Routes
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { getTodoData } from './TodoAPI.js'
-import AddTodo from './AddTodo.js';
+// import AddTodo from './AddTodo.js';
  
 export default class TodoApp extends Component {
     //set global state for todos here
-
+        state = { 
+        storedTodos: [] 
+    }
+    
     //add componentDidMount and get Todo data
-    //Update state with todos
+    async componentDidMount() {
+        //hit get route from API using deployed Heroku 
+        const todoData = await (getTodoData);
+
+        console.log(todoData.body);
+
+        //Update state with todos from API
+        this.setState({ storedTodos: todoData.body })
+    }
+
 
     //add handleClick function for new Todo
     //call post route and update state with new todo item
@@ -18,6 +30,15 @@ export default class TodoApp extends Component {
 
         return (
             <div>
+                <header> To Do List!</header>
+
+                <main>
+                    <div>
+                        {this.state.storedTodos.map(eachTodo =>
+                            <p></p> )}
+                    </div>
+
+                </main>
                  {/* //map over todos 
         //onClick mark todo as complete and update state
         
